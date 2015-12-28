aeb948ddd8031dea06185a9b8cb28a4ead40a585
belovamarina <belova.ma.ev@gmail.com>
2015-12-28 20:08:34 +0300

    Create LineItem scaffold

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
+
+	def session_count
+ 	 session[:counter] ||= 0
+  	 session[:counter] += 1
+	end
+
   def index
+  	@session = session_count
   	@products = Product.order(:title)
+
   end
 end
