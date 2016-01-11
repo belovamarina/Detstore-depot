b4080f4b949df11ec5d06d30ca15918c9d20ac76
belovamarina <belova.ma.ev@gmail.com>
2016-01-11 11:37:01 +0300

    Add RSS and JSON view for orders

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,19 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+        format.xml { render :xml => @product.to_xml(:include => :orders) }
+        format.json {render :json => @product.to_json(:include => :orders)}
+        format.html
+      end
+    end
+  end
+
   # GET /products
   # GET /products.json
   def index
